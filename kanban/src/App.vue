dadec1de654fdff11cd99231438331bb284933e8
emanuel12012 <emanuel.cafloriano@gmail.com>
2022-05-07 16:59:32 -0300

    firt commit

---
+++ kanban/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="contaioner mt-5"
+    <div class="row">
+     <div class="col form-inline">
+        <b-form-input v-model="newTask" placeholder="Enter Task" @keyup.enter="add"></b-form-input>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import draggable from "vuedraggable";
+name: 'App',
+components; {
+  draggable
+},
+data(){
+  return {
+    newTask: "",
+    arrBacklog: [
+      {name: "Code Sign Up Page"},
+      {name: "Test Dashboard"},
+      {name: "Style Registation"},
+      {name: "Helo with Designs"},
+    ],
+    arrInProgress:[],
+    arrTested:[],
+    arrDone: []
+  }
+},
+methods:{
+  add() {
+    if(this.newTask) {
+      this.arrBacklog.push({name: this.newTask  });
+      this.newTask="";
+    }
+  }
+}
+</script>
+
+<style>
+</style>
